fcfcd8c0a6fb189550322770bdb62017cf3da829
Alejandro Cea <alejcn35@gmail.com>
2020-11-18 14:24:37 +0000

    añadido interfaz gráfica

--- src/main/es/alejandro/Cliente.java
+++ src/main/es/alejandro/Cliente.java
@@ -2,39 +2,58 @@ package main.es.alejandro;
 
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class Cliente {
-	static String HOST = "localhost";
-	static int PUERTO = 5000;
-	Socket sc;
-	DataOutputStream salida;
-	DataInputStream entrada;
-	String mensajeRecibido;
-
-	public void initCliente() {
-		Scanner teclado = new Scanner(System.in);
-		try {
-			sc = new Socket(HOST, PUERTO);
-			salida = new DataOutputStream(sc.getOutputStream());
-			entrada = new DataInputStream(sc.getInputStream());
-			String msn = "";
-			while (!msn.equals("exit")) {
-				System.out.println("Cliente: Escriba un mensaje para enviar");
-				msn = teclado.nextLine();
-				salida.writeUTF(msn);
-				mensajeRecibido = entrada.readUTF();
-				System.out.println("Cliente: He recibido del Servidor: " + mensajeRecibido);
-			}
-			sc.close();
-			teclado.close();
-		} catch (Exception e) {
-			System.out.println("ERROR : " + e);
-		}
-	}
-
 	public static void main(String[] args) {
-		Cliente o = new Cliente();
-		o.initCliente();
+		MarcoCliente mimarco = new MarcoCliente();
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}	
+}
+
+class MarcoCliente extends JFrame {
+	public MarcoCliente() {
+		setBounds(600,300,280,350);
+		LaminaMarcoCliente milamina = new LaminaMarcoCliente();
+		add(milamina);
+		setVisible(true);	
+	}
+}
+
+class LaminaMarcoCliente extends JPanel {
+	private JTextField campo1;
+	private JButton miboton;
+	
+	public LaminaMarcoCliente() {
+		JLabel texto = new JLabel("Cliente");
+	
+		add(texto);
+		campo1 = new JTextField(20);
+		add(campo1);
+		miboton = new JButton("Enviar");
+		EnviarTexto mievento = new EnviarTexto();
+		miboton.addActionListener(mievento);
+		add(miboton);
+
+	}
+	
+	private class EnviarTexto implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try {
+				Socket socket = new Socket("localhost", 9999); //Se construye el socket
+				DataOutputStream flujoSalida = new DataOutputStream(socket.getOutputStream()); //Se crea el flujo
+				flujoSalida.writeUTF(campo1.getText()); //Mete dentro del flujo lo que escribamos
+				flujoSalida.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+			//System.out.println(campo1.getText());
+		}
+		
 	}
 }
--- src/main/es/alejandro/Servidor.java
+++ src/main/es/alejandro/Servidor.java
@@ -2,47 +2,52 @@ package main.es.alejandro;
 
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
 public class Servidor {
-	static int PUERTO = 5000;
-	ServerSocket sc;
-	Socket so;
-	DataOutputStream salida;
-	DataInputStream entrada;
-	String mensajeRecibido;
+	
+	public static void main(String[] args) {
+		MarcoServidor mimarco = new MarcoServidor();
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	
+	
+}
 
-	public void initServidor() {
 
-		Scanner teclado = new Scanner(System.in);
+class MarcoServidor extends JFrame implements Runnable{
+	public MarcoServidor() {
+		setBounds(1200,300,280,350);
+		JPanel milamina = new JPanel();
+		milamina.setLayout(new BorderLayout());
+		areatexto = new JTextArea();
+		milamina.add(areatexto, BorderLayout.CENTER);
+		add(milamina);
+		setVisible(true);
+		Thread hilo = new Thread(this);
+		hilo.start();
+	}
+	private JTextArea areatexto;
+	
+	@Override
+	public void run() {
+		//System.out.println("Estoy a la escucha");	
 		try {
-			sc = new ServerSocket(PUERTO);
-			so = new Socket();
-
-			System.out.println("Servidor: Esperando conexión...");
-			so = sc.accept();
-			System.out.println("Servidor: Se ha conectado un cliente...");
-			entrada = new DataInputStream(so.getInputStream());
-			salida = new DataOutputStream(so.getOutputStream());
-			String msn = "";
-			while (!msn.equals("exit")) {
-
-				mensajeRecibido = entrada.readUTF();
-				System.out.println("Servidor: Me ha enviado el Cliente: " + mensajeRecibido);
-				System.out.println("Servidor: Escriba un mensaje para enviar al Cliente");
-				msn = teclado.nextLine();
-				salida.writeUTF("" + msn);
+			ServerSocket servidor = new ServerSocket(9999); // Abrimos una conexión a través del puerto, en este caso 9999
+			while(true) {
+				Socket socket = servidor.accept(); //Acceptamos todas las conexiones
+				DataInputStream flujoEntrada = new DataInputStream(socket.getInputStream()); //Leemos lo que viene en el flujo de datos
+				String mensajeTexto = flujoEntrada.readUTF(); //Guardamos el flujo de datos del cliente
+				areatexto.append("\n" + mensajeTexto); //Imprimimos el texto del cliente en el area de texto
+				socket.close(); //cerramos la conexión
+				//servidor.close();
 			}
-			System.out.println("Servidor: Cerrando conexion: " + mensajeRecibido);
-			sc.close();
-			teclado.close();
-		} catch (Exception e) {
-			System.out.println("Servidor: Exception: " + e);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-
-	public static void main(String[] args) {
-		Servidor o = new Servidor();
-		o.initServidor();
-	}
 }
+	
